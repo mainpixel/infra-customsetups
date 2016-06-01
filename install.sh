af0460167b0b7754b31a674eb6e6873397493a4b
Patrick van Haren <info@patrickvanharen.nl>
2016-06-01 12:28:51 +0200

    comments install file

--- install.sh
+++ install.sh
@@ -13,19 +13,19 @@ then
 
 
     #update repos
-    #apt-get update
+    apt-get update
 
     #install default tools
-    #apt-get install -y nano vim htop telnet git curl gcc openssh-server net-tools mlocate
+    apt-get install -y nano vim htop telnet git curl gcc openssh-server net-tools mlocate
 
     #enable ssh root login
-    #sed -i -e 's/^PermitRootLogin/#PermitRootLogin/g' /etc/ssh/sshd_config
+    sed -i -e 's/^PermitRootLogin/#PermitRootLogin/g' /etc/ssh/sshd_config
 
     #run setup
     source setups/$1.sh
   
     #reboot te load configs
-    #reboot
+    reboot
 
   fi
 else
