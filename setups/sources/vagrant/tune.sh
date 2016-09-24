8cefef8941222a1c865fe1a16623cce617cbf699
Patrick van Haren <patrick@mainpixel.io>
2016-09-24 13:32:06 +0200

    vagrant issues

--- setups/sources/vagrant/tune.sh
+++ setups/sources/vagrant/tune.sh
@@ -2,4 +2,6 @@ cd /home/vagrant/Code
 mongo < createUser
 sudo killall -9 mongod
 sudo sed -i -e 's/bindIp/#bindIp/g' /etc/mongod.conf
-sudo /usr/bin/mongod -quiet --auth --config /etc/mongod.conf &
+sudo /usr/bin/mongod -quiet --auth --fork --config /etc/mongod.conf
+echo "sudo /usr/bin/mongod -quiet --auth --fork --config /etc/mongod.conf" > /etc/rc.local
+echo "exit 0" >> /etc/rc.local
