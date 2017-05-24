97c9a6908851b5aac860feea7833f12c03db87f4
Patrick van Haren <patrick@mainpixel.io>
2017-05-24 14:26:23 +0200

    test

--- files/updatessl.sh
+++ files/updatessl.sh
@@ -5,9 +5,9 @@ cd /root/.getssl
 for domain in `find * -maxdepth 0 -type  d`
 do
   echo $domain
-  cat $domain/$domain.crt > /etc/nginx/ssl2/$domain.pem
-  cat $domain/chain.crt >> /etc/nginx/ssl2/$domain.pem
-  cat $domain/$domain.key > /etc/nginx/ssl2/$domain.key
+  cat $domain/$domain.crt > /etc/nginx/ssl/$domain.pem
+  cat $domain/chain.crt >> /etc/nginx/ssl/$domain.pem
+  cat $domain/$domain.key > /etc/nginx/ssl/$domain.key
 done
 
 service nginx reload
